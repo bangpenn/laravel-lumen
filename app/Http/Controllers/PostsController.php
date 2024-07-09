a5f42843e156b79c4a5bba6de3cc1cfac3cfe1f8
bangpenn <fendikrama56@gmail.com>
2024-07-09 11:44:17 +0700

    feat:make crud

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+
+        return response()->json([
+            'success' => true,
+            'message' =>'List Semua Post',
+            'data'    => $posts
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title'   => 'required',
+            'content' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Semua Kolom Wajib Diisi!',
+                'data'   => $validator->errors()
+            ],401);
+
+        } else {
+
+            $post = Post::create([
+                'title'     => $request->input('title'),
+                'content'   => $request->input('content'),
+            ]);
+
+            if ($post) {
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Post Berhasil Disimpan!',
+                    'data' => $post
+                ], 201);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Post Gagal Disimpan!',
+                ], 400);
+            }
+
+        }
+    }
+
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        if ($post) {
+            return response()->json([
+                'success'   => true,
+                'message'   => 'Detail Post!',
+                'data'      => $post
+            ], 200);
+        } else {
+            return response()->json([
+                'success'   => false,
+                'message'   => 'Post Tidak Ditemukan!',
+            ], 404);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success'   => false,
+                'message'   => 'Semua kolom wajib diisi!',
+                'data'      => $validator->errors()
+            ], 401);
+        } else {
+            $post = Post::whereId($id)->update([
+                'title'     => $request->input('title'),
+                'content'   => $request->input('content'),
+            ]);
+
+            if ($post) {
+                return response()->json([
+                    'success'   => true,
+                    'message'   => 'Post berhasil diupdate!',
+                    'data'      => $post
+                ], 201);
+            } else {
+                return response()->json([
+                    'success'   => false,
+                    'message'   => 'Post gagal diupdate!'
+                ], 400);
+            }
+        }
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::whereId($id)->first();
+                    $post->delete();
+        
+        if ($post) {
+            return response()->json([
+                'success'   => true,
+                'message'   => 'Post berhasil dihapus!',
+            ], 200);
+        }
+    }
+}
--- database/migrations/2024_07_09_033004_create_posts_table.php
+++ database/migrations/2024_07_09_033004_create_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id('id');
             $table->string('title');
             $table->text('content');
             $table->timestamps();
